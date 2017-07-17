5056b384fd7b3a821b10a861e9fe94ac07a9cfaa
Joshua Foxworth <jfoxworth@cadwolf.com>
2017-07-17 13:58:09 -0500

    plan fixes

--- resources/views/sections/plan/main.blade.php
+++ resources/views/sections/plan/main.blade.php
@@ -4,15 +4,15 @@
         <h3>The plan</h3>
     </div>
 
-    <p>This page will not appear on the actual campaign site. It is simply a means of establishing a basic plan and setting reasonable markers for progress. It isn't set in stone, just a starting point.</p>
+    <p>Neither this page nor the link in the header will appear on the actual campaign site. It is simply a means of establishing a basic plan and setting reasonable markers for progress. It's just for us and it's just a starting point. FYI, this isn't me in the pictures, they are just place holders.</p>
 
-    <p>The plan has four main steps. The first is to establish footing with social media presence and just to make sure that we aren't in completely over our heads. The second step is to start a series of interviews on friendly media and begin to raise funds. The third step is to push into conservative mainstream media and push fundraising from mainstream sources. The fourth is the final phase of the campaign in Jan, Feb of 2018.</p>
+    <p>The plan has four main steps. The first is to establish footing with a social media presence - primarily to make sure that we aren't in completely over our heads. The second step is to start a series of interviews on friendly media and begin to raise funds. Here, we will begin to target friendly demographics in the distict directly. The third step is to push into conservative mainstream media and push fundraising from mainstream sources. We will not do large events unless funding is massive. We will stick to finding loyal voters and pushing them to the polls in March. The fourth is the final phase of the campaign in Jan, Feb of 2018.</p>
 
-    <p>I do not believe that most people pay attention to primaries at all. Those that do only do so in the days or weeks before the election. The overall strategy is to build the base in preparation for an onslaught attempt in the last few months. I feel like attempting to get attention early will not be enough to force a debate (although it's possible), and that establishing a high burn rate too early will cause the campaign to run out of funds in Jan/Feb.</p>
+    <p>I do not believe that most people pay attention to primaries at all. Those that do only do so in the days or weeks before the election. The overall strategy is to build the base in preparation for an onslaught attempt in the last few months. I feel like spending resources early will not be enough to force a debate (although it's not impossible), and that establishing a high burn rate too early will cause the campaign to run out of funds in Jan/Feb.</p>
 
-    <p>The strategy is to position our people so that we can get volunteers and paid staffers that can come do events in February. Give them time to get off of work and help, etc. Let me know if you feel this is a mistake. Obviously, if we take off in funds and polls, we can always add people and resources. I plan to run this like a startup and grab quality people and wait for the right time instead of amassing resources early.</p>
+    <p>The strategy is to collect and position our people so that we can get volunteers and paid staffers that can come do events in February. We give them enough warning so that they can get off of work and help, etc. Obviously, if we take off in funds and polls, we can always add people and resources. I plan to run this like a startup and grab quality people and wait for the right time instead of amassing resources early.</p>
 
-    <p>To this end, the early months will be spent establishing a bank of data on the web so that when people do their research in the days leading up to the election, they will see that we have a heavy presence - which will hopefully translate to a belief that we are a legitimate candidacy.</p>
+    <p>To this end, the early months will be spent establishing a bank of interviews and articles on the web so that when people do their research in the days leading up to the election, they will see that we have a heavy presence - which will hopefully translate to a belief that we are a legitimate candidacy with an honest chance at winning.</p>
 
     <p>We will focus heavily on our target demographic and emphasize getting a high turnout rate from key people. We will not go into poor demographic areas and attempt to convince them to treat us fairly.</p>
 
@@ -24,7 +24,7 @@
 
     <h4>Goals</h4>
 
-    <p>The objective of phase one is to establish a foothold on social media and determine where we stand. How much social media attention can we get? Can we get mainstream people to pay attention at all? Are there any normies that are paying attention and willing to support our views? This will likely be from mid August to mid September.</p>
+    <p>The objective of phase one is to establish a foothold on social media and determine where we stand by answering basic questions. How much social media attention can we get? Can we get mainstream people to pay attention at all? Are there any normies that are paying attention and willing to support our views? This will likely be from mid August to mid September.</p>
 
     <h4>Opposition</h4>
 
@@ -46,9 +46,9 @@
 
     <h4>Goals</h4>
 
-    <p>The goal in the second phase is to establish a following that can't be ignored and to begin canvassing the district to start encouraging voters to actually show up in March for the primary. In this phase, we should have enough money for some basic polling and we should be able to break the 10-15% threshold. However, polling isn't something that should concern us except for getting us into a debate.</p>
+    <p>The goal in the second phase is to establish a following that can't be ignored and to begin canvassing the district to start encouraging voters to actually show up in March for the primary. In this phase, we should have enough money for some basic polling and we should be able to break the 10-15% threshold. However, polling isn't something that should concern us except for possibly getting us into a debate.</p>
 
-    <p>The objective is to establish a bank of interviews and articles so that when a potential voter googles my name, they get the sense that the campaign is legitimate, and they have videos to watch that will show my views and what issues I consider to be primary. They should walk away from those videos and from the campaign site with the singular thought that Europeans in America are being treated unequally and that we have the right to demand our own country, to reverse immigration, and to end the corruption of our children into believing that they and their heritage are evil.</p>
+    <p>The objective is to establish a bank of interviews and articles so that when a potential voter googles my name, they get the sense that the campaign is legitimate, and they have videos to watch that will show my views and what issues I consider to be primary. They should walk away from those videos and from the campaign site with the singular thought that Europeans and America are being treated unequally and that we have the right to demand our own country, to reverse immigration, and to end the corruption of pushing our children into believing that they and their heritage are evil.</p>
 
     <p>We should also have the funds to set up an office. I will need to set up an official team too. The timeframe for this is Mid September to Mid November.</p>
 
@@ -58,7 +58,7 @@
 
     <h4>Strategy</h4>
 
-    <p>The goal is to get on friendly internet shows where we know that we will be treated fairly and where the conversation is reasonable and relaxed. These are shows like Stephan Molyneaux, Red Ice Radio, etc. Writing some articles for some of the more mainstream publications would be great too. However, no compromising on the core issues to get this. The regular media is to be shunned at this point and throughout most of the campaign. No interviews, no discussions, no access.</p>
+    <p>The goal is to get on friendly internet shows where we know that we will be treated fairly and where the conversation is reasonable and relaxed. These are shows like Stephan Molyneaux, Red Ice Radio, etc. Writing some articles for some of the more mainstream publications would be great too. However, no compromising on the core issues to get this. The regular media is to be shunned at this point and throughout most of the campaign. No interviews, no discussions, no access. I have written for American Thinker in the past in an effort to promote a site I was working on.</p>
 
     <p>During his campaign, Senator Mike Lee rented what appeared to be a university class room. It had sloped auditorium style seating and a chalk board at the front. He filed the room with supporters and discussed his plans as if he was teaching to a class. I found this to be very powerful and would like to do something similar. It provides a good starting point for people coming to the issues.</p>
 
@@ -110,7 +110,7 @@
 
     <p>In 2016, he won against a weak primary challenger 84% to 16% with only 68,000 people voting. He won the general election 62% to 38% with 260,000 people voting.</p>
 
-    <p>With no Presidential election to draw in people for the primary, I expect that the turnout will be less than 75,000 people. That means that we need to convince 35,000 people to vote for me. Many of those voting will vote for the incumbent no matter what, so we have to drive turnout. Convince 25,001 of that original 75,000 to vote for me and convince another 25,001 who don't normally vote in the primary to do so and we win.</p>
+    <p>With no Presidential election to draw in people for the primary, I expect that the turnout will be less than 75,000 people. That means that we need to convince 35,001 people to vote for me. Many of those that vote consitently will vote for the incumbent no matter what, so we have to drive turnout. Convince 25,001 of that original 75,000 to vote for me and convince another 25,001 who don't normally vote in the primary to do so and we win.</p>
 
 
 
@@ -118,7 +118,7 @@
         <h3>The Man</h3>
     </div>
 
-    <p>Randy Weber has never faced a serious opponent in his life. He simply does what he is told. He is not fast on his feet and not a good public speaker. I believe that if he is forced to respond, he will injure himself. The ultimate goal must be to force him to answer the questions as to why my sons must be placed behind the sons of other men for education purposes. Force him to answer why I must pay a minority owned subcontractor a portion of my salary in order to work at NASA. Force him to answer why other men are entitled to legal protection and I am not. Force him to answer why he states that he opposes amnesty and yet makes no real effort to stop immigration.</p>
+    <p>Randy Weber has never faced a serious opponent in his life. He simply does what he is told. He is not fast on his feet and not a good public speaker. I believe that if he is forced to respond, he will injure himself. The ultimate goal must be to force him to answer the questions on immigration and racial benefits. Force him to answer why I must pay a minority owned subcontractor a portion of my salary in order to work at NASA. Force him to answer why other men are entitled to legal protection and I am not. Force him to answer why he states that he opposes amnesty and yet makes no real effort to stop immigration.</p>
 
     <p>To this end, I have been visiting his office about once a week for the past few months and signing his ledger asking to speak with him. I have filmed this effort and plan to make a video out of the montage.</p>
 
