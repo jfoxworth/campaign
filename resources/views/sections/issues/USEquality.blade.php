59634aa1d67a1280be97de3ec20daca778704709
Joshua Foxworth <jfoxworth@cadwolf.com>
2017-08-23 15:29:24 -0500

    soften language

--- resources/views/sections/issues/USEquality.blade.php
+++ resources/views/sections/issues/USEquality.blade.php
@@ -27,8 +27,8 @@
 
     <div class="col_one_third col_last">
         <div class="feature-box fbox-center fbox-effect">
-            <h3>Equality for US Citizens</h3>
-            <p>The US population has the right to retain its culture</p>
+            <h3>A New Immigration System</h3>
+            <p>Easier for our citizens to bring in spouses, impossible for terrorists to get in</p>
         </div>
     </div>
 
@@ -48,16 +48,18 @@
 
     <p>Nations are formed by distinct people groups. The structure and character of that government reflects the culture of the people that created it. The presence of significant numbers of different peoples with different cultures changes the character of that nation and has always led to conflict. The US recognizes the right of all other nations to retain their culture. This includes the nations that we invade in response to terrorist attacks and the nations known to have committed crimes against their own people. We must reserve this right for ourselves as well.</p>
 
-    <p>This European, Christian culture native to this country is one of the most generous and giving the world has ever seen. Our citizens travel to distant lands to spread the Gospel and to build infrastructure to provide safe water to people they have never met. They adopt and raise the children of other people both outside of our nation and from the minority groups within our nation. It is a culture of small government, freedom, high trust, and low crime. It is a nation of men willing to plant trees, the shade of which they will never sit under. The continued existence of this culture benefits the world. The removal of this culture through "multi-culturalism" removes this benefit.</p>
+    <p>The culture native to this country is one of the most generous and giving the world has ever seen. Our citizens travel to distant lands to spread the Gospel and to build infrastructure to provide safe water to people they have never met. They adopt and raise the children of other people both outside of our nation and from the minority groups within our nation. It is a culture of small government, freedom, high trust, and low crime. It is a nation of men willing to plant trees, the shade of which they will never sit under. The continued existence of this culture benefits the world. The removal of this culture through "multi-culturalism" removes this benefit.</p>
 
-    <p>The character of our nation and our laws has reflected this culture. The movement of tens of millions of people from other cultures into our lands changes that culture. To assert that the native population of this country has no right to deny this migration while other nations retain this right is to assert that our culture is either inferior and therefore worthy of replacement or that it is inherently evil and therefore in need of replacement. Neither of those options bode well for the treatment of the native population.</p>
+    <p>The character of our nation and our laws has reflected this culture. The movement of tens of millions of people from other cultures into our lands changes that culture and the government reflective of it. To assert that the native population of this country has no right to deny this migration while other nations retain this right is to assert that our culture is either inferior and therefore worthy of replacement or that it is inherently evil and therefore in need of replacement. Neither of those options bode well for the treatment of the native population by the incoming population.</p>
 
     <blockquote>
         <p>To assert that the native white population of this country has no right to deny this migration while other nations and peoples retain this right is to assert that our culture is either inferior and worthy of replacement or inherently evil and in need of replacement. Neither of those options bode well for the treatment of the native population.</p>
         <footer>Joshua Foxworth</footer>
     </blockquote>
 
-    <p>Our culture has a right to exist. We have a right to self determination. We have a right to deny immigration to any and all people we see fit and to repatriate any who are present as the result of entering illegally. To question these rights is to question our right to equality.</p>
+    <p>Our culture has a right to exist. We have a right to self determination. We have a right to deny immigration to any and all people we see fit and to repatriate any who are present as the result of entering illegally. To question these rights is to question our right to equality.</p> 
+
+    <p>It is time for a new immigration policy. One that honors the cultures of our friends while retaining ours.</p>
 
 
     <div class="fancy-title nobottommargin">
@@ -77,9 +79,39 @@
 
     <p>Twenty years later, the American people demanded that something be done to end the unprecedented levels of legal and illegal immigration into their country. In 1986, President Ronald Reagan signed the Immigration Reform and Control Act, also known as the Simpson–Mazzoli Act. The act was a "grand bargain" where all illegal aliens already present in the US were given citizenship with the promise to Americans that no more would be allowed to enter and US laws would be enforced.</p>
 
-    <p>This again was another lie to the American people and the borders were left wide open to allow in tens of millions more illegal aliens. Since that time, every American president has campaigned on the promise of ending illegal immigration and every single one has acted in the exact opposite manner once elected.</p>
+    <p>This agreement was not honored and the borders were left wide open to allow in tens of millions more illegal aliens. Since that time, every American president has campaigned on the promise of ending illegal immigration and every single one has acted in the exact opposite manner once elected.</p>
 
-    <p>The history of the mass immigration of the last 50 years is one of illegal activity, corrupt politicians, and broken promises to the American people. The American people are under no moral nor legal obligation to grant citizenship to anyone who entered under such conditions - quite the opposite.</p>
+    <p>The history of the mass immigration of the last 50 years is one of illegal activity, corrupt politicians, and broken promises to the American people. The American people are under no moral nor legal obligation to grant citizenship to anyone who entered under such conditions.</p>
+
+
+
+
+    <div class="fancy-title nobottommargin">
+        <h3>A new Immigration Policy - Enforce US immigration laws, end mass migration, change who comes in</h3>
+    </div>
+
+    <div class="divider divider-border divider-right notopmargin">
+        <i class="icon-flag-alt"></i>
+    </div>
+
+    <p>A new immigration plan must consist of two main pillars - enforce valid US laws and end mass migration. To implement the first pillar, no laws need to be passed as every law needed is already in place, we need only enforce them. These are the same laws that every President since Reagan has pledged to enforce.</p>
+
+    <p>By enforcing laws that punish employers that hire illegal aliens, we encourage those people to return home. Refusing welfare to newly arrived and illegal aliens will do the same. We must also ensure that the status of every person is checked when they access the health care system, education system, or any government system like licenses to drive.</p>
+
+    <p>Any person whose presence is the result of illegal entry must be legally removed. It is not immoral for us to enforce our laws. It is not unethical for us to assert equality for our people. Any assertion that it is unethical for us to enforce our laws, but completely acceptable for nations like Japan, China, Israel, Saudia Arabia, Mexico, or any African country, is an assertion that we are a people unworthy of equality - unworthy of our own nations. Any representative that works on behalf of the people of other nations to bring them to our lands betrays their oath of office.</p>
+
+    <blockquote>
+        <p>Any assertion that it is unethical for us to enforce our laws, but completely acceptable for nations like Japan, China, Israel, Saudia Arabia, Mexico, or any African country, is an assertion that we are a people unworthy of equality - unworthy of our own nations.</p>
+        <footer>Joshua Foxworth</footer>
+    </blockquote>
+
+    <p>Once the US government is again in control of who comes into and remains within the country, we must end the concept of replacement migration. The people of the US have always felt a moral need to give refuge to those in danger. However, there is a drastic moral difference between giving someone refuge from a war and the mass importation of millions of people under the guise of a refugee program. The practice of providing permanent resettlement for people fleeing a dangerous situation must end. Those brought here under those conditions must be returned to their native lands once those lands are safe.</p>
+
+    <p>The practice of importing cheap labor from the third world must end. The American people are well educated, moral, and capable of handling any technical need that arises in our companies.</p>
+
+    <p>In addition to this, the legal process must be drastically altered. We must end the concept of chain migration - where one person inside the country is allowed to bring in all of their relatice, who are in turn allowed to do the same. We must end all migration from countries and cultures hostile to us, and we must make it much easier for spouses of US citizens to enter the US and remain here as citizens.</p>
+
+
 
 
     <div class="fancy-title nobottommargin">
@@ -90,7 +122,7 @@
         <i class="icon-flag-alt"></i>
     </div>
 
-    <p>The damage done to the culture and our economy has been catastrophic. In recent years, the amount of money sent to Mexico from workers in the US has surpassed the money brought into that country by oil revenue. This loss of financial capital to the US alone should be enough to spur action.</p>
+    <p>The damage done by mass immigration to our culture and our economy has been catastrophic. In recent years, the amount of money sent to Mexico from workers in the US has surpassed the money brought into that country by oil revenue. This loss of financial capital to the US alone should be enough to spur action.</p>
 
     <p>In addition to this, <a href="https://cis.org/Welfare-Use-Legal-and-Illegal-Immigrant-Households">49% of households headed by legal immigrants</a> used one or more welfare programs in 2012, compared to 30 percent of households headed by natives. Households headed by legal immigrants have higher use rates than native households overall and for cash programs (14 percent vs. 10 percent), food programs (36 percent vs. 22 percent), and Medicaid (39 percent vs. 23 percent). Use of housing programs is similar. In addition to this, illegal immigrant households account for one quarter of all immigrant households accessing one or more welfare programs.</p>
 
@@ -102,7 +134,7 @@
 
 
 
-    <div class="fancy-title nobottommargin">
+<!--     <div class="fancy-title nobottommargin">
         <h3>An end to diversity</h3>
     </div>
 
@@ -113,62 +145,8 @@
     <p>The primary argument's for the importation of migrants are that they are economically enriching and that our culture needs "diversity". The reality is that both legal and illegal migrants use welfare at rates much higher than the native population and are far less educated. They are not a benefit to the social and welfare systems, but rather a drain on those systems.</p>
 
     <p>This second argument, the call for "diversity", must be seen for what it is - an assertion of inequality. No city, no neighborhood, and no business is worthy of existing unless it contains less of the native population. We as a people are the only ones that are told that we must accept that our nations are simply better if there are fewer of us.</p>
-
+ -->
 <!--     <p>Africa is for the Africans, Asia is for the Asians, Central and South American are for the Latinos, but Europe, America and Canada simply must be multicultural. For us to retain our homelands is racist. To imply that any other people group not retain theirs is racist. All of this in the name of diversity, and under the banner that Europeans and Americans have a special debt to the remaining population. A debt that can only be paid by removing our right to self determination.</p>
  -->
-    <p>This inequality must be ended.</p>
-
-
-
-    <div class="fancy-title nobottommargin">
-        <h3>Enforce US immigration laws, end mass migration</h3>
-    </div>
-
-    <div class="divider divider-border divider-right notopmargin">
-        <i class="icon-flag-alt"></i>
-    </div>
-
-
-    <p>To solve this problem, no laws need to be passed as every law needed is already in place, we need only enforce them. These are the same laws that every President since Reagan has pledged to enforce.</p>
-
-    <p>By punishing employers that hire illegal aliens, those people will return home. By refusing welfare to newly arrived and illegal aliens, we will provide more incentive for these people to return home.</p>
-
-    <p>In addition to this, US tax payers cannot be forced to pay for the health care of other people - especially while our own costs are so high. Any person who accesses the health care system - even through an emergency - must be treated and then have their citizenship status ascertained and held for deportation if merited.</p>
-
-    <p>It is not immoral for us to enforce our laws. It is not unethical for us to assert equality for our people. Any assertion that it is unethical for us to maintain a homogenous nation, but completely acceptable for nations like Japan, China, Israel, Saudia Arabia, Mexico, or any African country, is an assertion that we are a people unworthy of equality - unworthy of our own nations. Any representative that works on behalf of the people of other nations to bring them to our lands betrays their oath of office.</p>
-
-    <blockquote>
-        <p>Any assertion that it is unethical for us to maintain a homogenous nation, but completely acceptable for nations like Japan, China, Israel, Saudia Arabia, Mexico, or any African country, is an assertion that we are a people unworthy of equality - unworthy of our own nations.</p>
-        <footer>Joshua Foxworth</footer>
-    </blockquote>
-
-
-    <div class="fancy-title nobottommargin">
-        <h3>An end to permanent refugee resettlement</h3>
-    </div>
-
-    <div class="divider divider-border divider-right notopmargin">
-        <i class="icon-flag-alt"></i>
-    </div>
-
-    <p>The people of the US have always felt a moral need to give refuge to those in danger. However, there is a drastic moral difference between giving someone refuge from a war and the mass importation of millions of people under the guise of a refugee program.</p>
-
-    <p>The practice of providing permanent resettlement for people fleeing a dangerous situation must end. Those brought here under those conditions must be returned to their native lands once those lands are safe.</p>
-
-
-
-
-
-
-    <div class="fancy-title nobottommargin">
-        <h3>An end to H1-B and H2-B visas</h3>
-    </div>
-
-    <div class="divider divider-border divider-right notopmargin">
-        <i class="icon-flag-alt"></i>
-    </div>
-
-
-    <p>The practice of importing cheap labor from the third world must end. The American people are well educated, moral, and capable of handling any technical need that arises in our companies.</p>
-
+    <!-- <p>This inequality must be ended.</p> -->
 </div>
