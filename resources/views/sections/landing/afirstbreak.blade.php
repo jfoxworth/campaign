d1b4bcf6af582d0420af7d164da9e6dfd7c1a233
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-08-17 14:42:12 -0500

    changing content

--- resources/views/sections/landing/afirstbreak.blade.php
+++ resources/views/sections/landing/afirstbreak.blade.php
@@ -2,6 +2,6 @@
 
     <div class="heading-block center ">
         <h2 class="t500 nott" style="font-size:36px:">"America First" Means <span>Americans</span> Come First</h2>
-        <span>Americans have a right to a nation of their own and a right to representatives that act on their behalf</span>
+        <span>We cannot allow a single dollar to be spent on the welfare, education, and health care of foreign citizens while our children, elderly, and veterans go without.</span>
     </div>
 </div>
--- resources/views/sections/landing/countdown.blade.php
+++ resources/views/sections/landing/countdown.blade.php
@@ -2,10 +2,12 @@
 <div class="promo topmargin-lg promo-border bottommargin-lg promo-full">
     <div class="container clearfix">
         <div class="col_one_third center nobottommargin">
-            <h3>The primary is March 6, 2018</h3>
+            <h3>The primary is March 3, 2020</h3>
         </div>
+        <!--
         <div class="col_two_third col_last center nobottommargin">
             <div id="countdownprimary"></div>
         </div>
+    -->
     </div>
 </div>
