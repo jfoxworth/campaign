f330647f962b6652b78d9ffe4754c54199ba9614
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-08-17 14:33:00 -0500

    changing content

--- resources/views/sections/landing/corePromiseBreak.blade.php
+++ resources/views/sections/landing/corePromiseBreak.blade.php
@@ -1,6 +1,14 @@
-<div class="promo promo-border promo-full">
+<div class="section dark">
+    
     <div class="container clearfix">
-        <h3>My Pledge - Results, not excuses</h3>
-        <span>Immigration, the budget, life, marriage, health care. No matter the issue, "conservatism" in America has become synonomous with "lose with dignity." I will break from this pattern. On the day that I am sworn in, I will lead a nation wide movement to enforce our immigration laws and end affrimative action. We will not allow the nation to carry out its business if the business if this nation is to circumvent our will and subjugate our children. </span>
+        
+        <div class="heading-block center nobottomborder nobottommargin"
+        
+     		<h3 class="t500 nott">My Pledge - Results, not excuses</h3>
+       
+    		<p class="t500 nott">Immigration, the budget, life, marriage, health care. No matter the issue, "conservatism" in America has become synonomous with "lose with dignity." I will break from this pattern. On the day that I am sworn in, I will lead a nation wide movement to enforce our immigration laws and end affrimative action. We will not allow the nation to carry out its business if the business if this nation is to circumvent our will and subjugate our children. </p>
+    
+    	</div>
     </div>
+
 </div>
