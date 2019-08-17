5a80eedf6e78a699ab42d009950bc19299208c1c
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-08-17 12:07:35 -0500

    changing content

--- resources/views/sections/landing/slider.blade.php
+++ resources/views/sections/landing/slider.blade.php
@@ -1,40 +1,38 @@
-        <div id="home" class="page-section" style="position:absolute;top:0;left:0;width:100%;height:200px;z-index:-2;"></div>
+		<div id="home" class="page-section" style="position:absolute;top:0;left:0;width:100%;height:200px;z-index:-2;"></div>
 
-        <section id="slider" class="slider-element slider-parallax full-screen with-header swiper_wrapper clearfix">
+		<section id="slider" class="slider-element slider-parallax full-screen with-header swiper_wrapper clearfix">
 
-            <div class="slider-parallax-inner">
+			<div class="slider-parallax-inner">
 
-                <div class="swiper-container swiper-parent">
-                    <div class="swiper-wrapper">
-                        <div class="swiper-slide dark" style="background-image: url('images/landing/campaign1.png');">
-                            <div class="container clearfix">
-                                <div class="vertical-middle dark center">
+				<div class="swiper-container swiper-parent">
+					<div class="swiper-wrapper">
+						<div class="swiper-slide dark" style="background-image: url('images/landing/campaign1.png');">
+							<div class="container clearfix">
+								<div class="vertical-middle dark center">
 
-                                    <div class="col_half center">
-                                        <div class="logoStarLine">
-                                            <div class="logoStar">
-                                                <i class="icon-star i-alt"></i>
-                                                <i class="icon-star i-alt"></i>
-                                                <i class="icon-star i-alt"></i>
-                                            </div>
-                                        </div>
-                                        <i class="icon-star i-alt"></i>
-                                        <i class="icon-star i-alt"></i>
-                                        <div class="mainLogoText">Foxworth for Congress</div>
-                                        <div class="secondaryLogoText">Enforce Immigration Laws</div>
-                                        <div class="secondaryLogoText">End Affirmative Action</div>
-                                        <div class="secondaryLogoText">Restore the Nation</div>
-                                        <div class="heading-block nobottommargin center"></div>
+									<div class="col_half center">
+										<div class="center">
+											<div class="center logoStarLine">
+												<div class="logoStar"><i class="icon-star i-alt"></i></div>
+												<div class="logoStar"><i class="icon-star i-alt"></i></div>
+												<div class="logoStar"><i class="icon-star i-alt"></i></div>
+											</div>
+											<div class="mainLogoText">Foxworth for Congress</div>
+											<div class="secondaryLogoText">Enforce Immigration Laws</div>
+											<div class="secondaryLogoText">End Affirmative Action</div>
+											<div class="secondaryLogoText">Restore the Nation</div>
+											<div class="heading-block nobottommargin center"></div>
+										</div>
 
 
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 
-            </div>
+			</div>
 
-        </section>
+		</section>
 
