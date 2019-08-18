0148d297e302ed077b28cd31ecfafb357ca8762b
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-08-18 18:22:04 -0500

    changing content

--- resources/views/sections/issues/issueportfolio.blade.php
+++ resources/views/sections/issues/issueportfolio.blade.php
@@ -1,162 +1,308 @@
 <div class="container clearfix">
 
-    <div id="portfolio" class="portfolio grid-container portfolio-nomargin portfolio-full portfolio-overlay-open clearfix">
-
-        <article class="portfolio-item pf-media pf-icons">
-            <div class="portfolio-image">
-                <a href="/Issues/Equality">
-                    <img src="/images/issues/equality.jpg" alt="Equality">
-                    <div class="portfolio-overlay">
-                        <div class="portfolio-desc">
-                            <h3>Equality</h3>
-                            <span>A general discussion on equality</span>
-                        </div>
-                    </div>
-                </a>
-            </div>
-        </article>
-
-        <article class="portfolio-item pf-illustrations">
-            <div class="portfolio-image">
-                <a href="/Issues/USEquality">
-                    <img src="/images/issues/usequality.jpg" alt="US Equality">
-                    <div class="portfolio-overlay">
-                        <div class="portfolio-desc">
-                            <h3>US Equality</h3>
-                            <span>An end to immigration</span>
-                        </div>
-                    </div>
-                </a>
-            </div>
-        </article>
-
-        <article class="portfolio-item pf-graphics pf-uielements">
-            <div class="portfolio-image">
-                <a href="/Issues/RacialEquality">
-                    <img src="/images/issues/racialequality.jpg" alt="Racial Equality">
-                    <div class="portfolio-overlay">
-                        <div class="portfolio-desc">
-                            <h3>Racial Equality</h3>
-                            <span>An end to preferences and punishmens</span>
-                        </div>
-                    </div>
-                </a>
-            </div>
-        </article>
-
-        <article class="portfolio-item pf-icons pf-illustrations">
-            <div class="portfolio-image">
-                <a href="/Issues/CongressEquality">
-                    <img src="/images/issues/congressequality.jpg" alt="Congressional Equality">
-                    <div class="portfolio-overlay">
-                        <div class="portfolio-desc">
-                            <h3>Congressional Equality</h3>
-                            <span>Reign in a Lawless Congress</span>
-                        </div>
-                    </div>
-                </a>
-            </div>
-        </article>
-
-        <article class="portfolio-item pf-uielements pf-media">
-            <div class="portfolio-image">
-                <a href="/Issues/HealthCare">
-                    <img src="/images/issues/healthcare.jpg" alt="Health Care">
-                    <div class="portfolio-overlay">
-                        <div class="portfolio-desc">
-                            <h3>Health Care</h3>
-                            <span>A common sense plan to health care</span>
-                        </div>
-                    </div>
-                </a>
-            </div>
-        </article>
-
-        <article class="portfolio-item pf-graphics pf-illustrations">
-            <div class="portfolio-image">
-                <a href="/Issues/Education">
-                    <img src="/images/issues/education.jpg" alt="Education">
-                    <div class="portfolio-overlay" data-lightbox="gallery">
-                        <div class="portfolio-desc">
-                            <h3>Education</h3>
-                            <span>Safe Schools, US Schools for US Citizens</span>
-                        </div>
-                    </div>
-                </a>
-            </div>
-        </article>
-
-        <article class="portfolio-item pf-uielements pf-icons">
-            <div class="portfolio-image">
-                <a href="/Issues/NASA">
-                    <img src="/images/issues/nasa.jpg" alt="NASA">
-                    <div class="portfolio-overlay">
-                        <div class="portfolio-desc">
-                            <h3>NASA</h3>
-                            <span>Back to the moon and then to Mars</span>
-                        </div>
-                    </div>
-                </a>
-            </div>
-        </article>
-
-        <article class="portfolio-item pf-graphics">
-            <div class="portfolio-image">
-                <a href="/Issues/CivilRights">
-                    <img src="/images/issues/civilrights.jpg" alt="Civil Rights">
-                    <div class="portfolio-overlay">
-                        <div class="portfolio-desc">
-                            <h3>Civil Rights</h3>
-                            <span>Restore our civil rights, remove terrorists</span>
-                        </div>
-                    </div>
-                </a>
-            </div>
-        </article>
+	<div id="portfolio" class="portfolio grid-container portfolio-overlay-open clearfix">
 
 
-        <article class="portfolio-item pf-graphics">
-            <div class="portfolio-image">
-                <a href="/Issues/SecondAmendment">
-                    <img src="/images/issues/secondamendment.jpg" alt="The Second Amendment">
-                    <div class="portfolio-overlay">
-                        <div class="portfolio-desc">
-                            <h3>Second Amendment</h3>
-                            <span>Defend the right to bear arms</span>
-                        </div>
-                    </div>
-                </a>
-            </div>
-        </article>
+		<article class="portfolio-item pf-media pf-icons">
+			<div class="portfolio-image">
+				<a href="/Issues/Equality">
+					<img src="/images/issues/immigration.jpg" alt="Immigration">
+					<div class="portfolio-overlay">
+						<div class="portfolio-desc">
+							<h3>Immigration</h3>
+							<span>End illegal immigration and re-evaluate legal immigration policies</span>
+						</div>
+					</div>
+				</a>
+			</div>
+		</article>
 
-        <article class="portfolio-item pf-graphics">
-            <div class="portfolio-image">
-                <a href="/Issues/ForeignPolicy">
-                    <img src="/images/issues/foreignpolicy.jpg" alt="Foreign Policy">
-                    <div class="portfolio-overlay">
-                        <div class="portfolio-desc">
-                            <h3>Foreign Policy</h3>
-                            <span>End the Wars, Stand with Europe</span>
-                        </div>
-                    </div>
-                </a>
-            </div>
-        </article>
+		<article class="portfolio-item pf-media pf-icons">
+			<div class="portfolio-image">
+				<a href="/Issues/Equality">
+					<img src="/images/issues/affirmativeaction.jpg" alt="Affirmative Action">
+					<div class="portfolio-overlay">
+						<div class="portfolio-desc">
+							<h3>Affirmative Action</h3>
+							<span>An immediate and unconditional end to these policies</span>
+						</div>
+					</div>
+				</a>
+			</div>
+		</article>
 
-        <article class="portfolio-item pf-graphics">
-            <div class="portfolio-image">
-                <a href="/Issues/ForeignPolicy">
-                    <img src="/images/issues/statue.jpg" alt="Monuments">
-                    <div class="portfolio-overlay">
-                        <div class="portfolio-desc">
-                            <h3>Monuments</h3>
-                            <span>Oppose the destruction of our history</span>
-                        </div>
-                    </div>
-                </a>
-            </div>
-        </article>
+		<article class="portfolio-item pf-media pf-icons">
+			<div class="portfolio-image">
+				<a href="/Issues/Equality">
+					<img src="/images/issues/restorethenation.jpg" alt="Restore The Nation">
+					<div class="portfolio-overlay">
+						<div class="portfolio-desc">
+							<h3>Restore the Nation</h3>
+							<span>Restore the 1st and 4th amendments and protect the 2nd amendment</span>
+						</div>
+					</div>
+				</a>
+			</div>
+		</article>
 
-    </div>
+
+		<article class="portfolio-item pf-media pf-icons">
+			<div class="portfolio-image">
+				<a href="/Issues/Equality">
+					<img src="/images/issues/congressionalethics.jpg" alt="Congressional Ethics">
+					<div class="portfolio-overlay">
+						<div class="portfolio-desc">
+							<h3>Congressional Ethics</h3>
+							<span>Reign in a lawless Congress</span>
+						</div>
+					</div>
+				</a>
+			</div>
+		</article>
+
+
+		<article class="portfolio-item pf-media pf-icons">
+			<div class="portfolio-image">
+				<a href="/Issues/HealthCare">
+					<img src="/images/issues/healthcare.jpg" alt="Health Care">
+					<div class="portfolio-overlay">
+						<div class="portfolio-desc">
+							<h3>Health Care</h3>
+							<span>A common sense plan to health care</span>
+						</div>
+					</div>
+				</a>
+			</div>
+		</article>
+
+
+		<article class="portfolio-item pf-media pf-icons">
+			<div class="portfolio-image">
+				<a href="/Issues/ForeignPolicy">
+					<img src="/images/issues/foreignpolicy.jpg" alt="Foreign Policy">
+					<div class="portfolio-overlay">
+						<div class="portfolio-desc">
+							<h3>Foreign Policy</h3>
+							<span>End the Wars, Stand with Europe</span>
+						</div>
+					</div>
+				</a>
+			</div>
+		</article>
+
+
+		<article class="portfolio-item pf-media pf-icons">
+			<div class="portfolio-image">
+				<a href="/Issues/SecondAmendment">
+					<img src="/images/issues/secondamendment.jpg" alt="The Second Amendment">
+					<div class="portfolio-overlay">
+						<div class="portfolio-desc">
+							<h3>Second Amendment</h3>
+							<span>Defend the right to bear arms</span>
+						</div>
+					</div>
+				</a>
+			</div>
+		</article>
+
+
+
+		<article class="portfolio-item pf-media pf-icons">
+			<div class="portfolio-image">
+				<a href="/Issues/NASA">
+					<img src="/images/issues/nasa.jpg" alt="NASA">
+					<div class="portfolio-overlay">
+						<div class="portfolio-desc">
+							<h3>NASA</h3>
+							<span>Back to the moon and then to Mars</span>
+						</div>
+					</div>
+				</a>
+			</div>
+		</article>
+
+
+
+
+		<article class="portfolio-item pf-media pf-icons">
+			<div class="portfolio-image">
+				<a href="/Issues/Education">
+					<img src="/images/issues/education.jpg" alt="Education">
+					<div class="portfolio-overlay" data-lightbox="gallery">
+						<div class="portfolio-desc">
+							<h3>Education</h3>
+							<span>Safe Schools, US Schools for US Citizens</span>
+						</div>
+					</div>
+				</a>
+			</div>
+		</article>
+
+	</div>
 
 </div>
+
+
+
+<!--
+		<article class="portfolio-item pf-media pf-icons">
+			<div class="portfolio-image">
+				<a href="/Issues/Equality">
+					<img src="/images/issues/equality.jpg" alt="Equality">
+					<div class="portfolio-overlay">
+						<div class="portfolio-desc">
+							<h3>Equality</h3>
+							<span>A general discussion on equality</span>
+						</div>
+					</div>
+				</a>
+			</div>
+		</article>
+
+		<article class="portfolio-item pf-illustrations">
+			<div class="portfolio-image">
+				<a href="/Issues/USEquality">
+					<img src="/images/issues/usequality.jpg" alt="US Equality">
+					<div class="portfolio-overlay">
+						<div class="portfolio-desc">
+							<h3>US Equality</h3>
+							<span>An end to immigration</span>
+						</div>
+					</div>
+				</a>
+			</div>
+		</article>
+
+		<article class="portfolio-item pf-graphics pf-uielements">
+			<div class="portfolio-image">
+				<a href="/Issues/RacialEquality">
+					<img src="/images/issues/racialequality.jpg" alt="Racial Equality">
+					<div class="portfolio-overlay">
+						<div class="portfolio-desc">
+							<h3>Racial Equality</h3>
+							<span>An end to preferences and punishmens</span>
+						</div>
+					</div>
+				</a>
+			</div>
+		</article>
+
+		<article class="portfolio-item pf-icons pf-illustrations">
+			<div class="portfolio-image">
+				<a href="/Issues/CongressEquality">
+					<img src="/images/issues/congressequality.jpg" alt="Congressional Equality">
+					<div class="portfolio-overlay">
+						<div class="portfolio-desc">
+							<h3>Congressional Equality</h3>
+							<span>Reign in a Lawless Congress</span>
+						</div>
+					</div>
+				</a>
+			</div>
+		</article>
+
+		<article class="portfolio-item pf-uielements pf-media">
+			<div class="portfolio-image">
+				<a href="/Issues/HealthCare">
+					<img src="/images/issues/healthcare.jpg" alt="Health Care">
+					<div class="portfolio-overlay">
+						<div class="portfolio-desc">
+							<h3>Health Care</h3>
+							<span>A common sense plan to health care</span>
+						</div>
+					</div>
+				</a>
+			</div>
+		</article>
+
+		<article class="portfolio-item pf-graphics pf-illustrations">
+			<div class="portfolio-image">
+				<a href="/Issues/Education">
+					<img src="/images/issues/education.jpg" alt="Education">
+					<div class="portfolio-overlay" data-lightbox="gallery">
+						<div class="portfolio-desc">
+							<h3>Education</h3>
+							<span>Safe Schools, US Schools for US Citizens</span>
+						</div>
+					</div>
+				</a>
+			</div>
+		</article>
+
+		<article class="portfolio-item pf-uielements pf-icons">
+			<div class="portfolio-image">
+				<a href="/Issues/NASA">
+					<img src="/images/issues/nasa.jpg" alt="NASA">
+					<div class="portfolio-overlay">
+						<div class="portfolio-desc">
+							<h3>NASA</h3>
+							<span>Back to the moon and then to Mars</span>
+						</div>
+					</div>
+				</a>
+			</div>
+		</article>
+
+		<article class="portfolio-item pf-graphics">
+			<div class="portfolio-image">
+				<a href="/Issues/CivilRights">
+					<img src="/images/issues/civilrights.jpg" alt="Civil Rights">
+					<div class="portfolio-overlay">
+						<div class="portfolio-desc">
+							<h3>Civil Rights</h3>
+							<span>Restore our civil rights, remove terrorists</span>
+						</div>
+					</div>
+				</a>
+			</div>
+		</article>
+
+
+		<article class="portfolio-item pf-graphics">
+			<div class="portfolio-image">
+				<a href="/Issues/SecondAmendment">
+					<img src="/images/issues/secondamendment.jpg" alt="The Second Amendment">
+					<div class="portfolio-overlay">
+						<div class="portfolio-desc">
+							<h3>Second Amendment</h3>
+							<span>Defend the right to bear arms</span>
+						</div>
+					</div>
+				</a>
+			</div>
+		</article>
+
+		<article class="portfolio-item pf-graphics">
+			<div class="portfolio-image">
+				<a href="/Issues/ForeignPolicy">
+					<img src="/images/issues/foreignpolicy.jpg" alt="Foreign Policy">
+					<div class="portfolio-overlay">
+						<div class="portfolio-desc">
+							<h3>Foreign Policy</h3>
+							<span>End the Wars, Stand with Europe</span>
+						</div>
+					</div>
+				</a>
+			</div>
+		</article>
+
+		<article class="portfolio-item pf-graphics">
+			<div class="portfolio-image">
+				<a href="/Issues/ForeignPolicy">
+					<img src="/images/issues/statue.jpg" alt="Monuments">
+					<div class="portfolio-overlay">
+						<div class="portfolio-desc">
+							<h3>Monuments</h3>
+							<span>Oppose the destruction of our history</span>
+						</div>
+					</div>
+				</a>
+			</div>
+		</article>
+
+	</div>
+
+</div>
+
+
+-->
--- resources/views/sections/landing/issueSummary.blade.php
+++ resources/views/sections/landing/issueSummary.blade.php
@@ -10,17 +10,17 @@
 
 					<div class="col_one_third">
 						<h4>Enforce Immigration Laws</h4>
-						<p>A nation is not valid if its laws can be ignored and its borders are irrelavant. The immediate and unconditional enforcement of valid US immigration is the core Republican issue. <a href="/Issues/USEquality">(more)</a></p>
+						<p>A nation is not valid if its laws can be ignored and its borders are irrelavant. The immediate and unconditional enforcement of valid US immigration is the core Republican issue. <a href="/Issues">(issues)</a></p>
 					</div>
 
 					<div class="col_one_third">
 						<h4>End Affirmative Action</h4>
-						<p>As a nation of equal citizens, the brutal and oppressive system of laws and policies collectively known as affirmative action must be immediately recognized for the violation of civil rights that they represent. <a href="/Issues/RacialEquality">(more)</a></p>
+						<p>As a nation of equal citizens, the brutal and oppressive system of laws and policies collectively known as affirmative action must be immediately recognized for the violation of civil rights that they represent. <a href="/Issues">(issues)</a></p>
 					</div>
 
 					<div class="col_one_third col_last">
 						<h4>Restore the Nation</h4>
-						<p>I will ensure that first amendment rights are enforced on the internet, that we do not waiver on the second amendment, and that we begin the work to restore the fourth amendment. <a href="/Issues/CongressEquality">(more)</a></p>
+						<p>I will ensure that first amendment rights are enforced on the internet, that we do not waiver on the second amendment, and that we begin the work to restore the fourth amendment. <a href="/Issues">(issues)</a></p>
 					</div>
 
 					<div class="clear"></div>
