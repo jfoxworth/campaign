f41940db54cd3a915241a156d4ce951254861301
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-08-18 20:18:37 -0500

    changing content

--- resources/views/sections/pledge/main.blade.php
+++ resources/views/sections/pledge/main.blade.php
@@ -7,7 +7,7 @@
 	    <span>
 	    	<p>Immigration, the budget, life, marriage, health care. No matter the issue, "conservatism" in America has become synonomous with "lose with dignity." I will break from this pattern.</p>
 
-	    	<p>A government that refuses to enforce the laws of the nation on foreign people while giving financial and legal preferences to those same foreign peoples over US citizens is not a legitimate government. My pledge to the constituents of congressional district 14 is that I will not allow the government to carry out its business if the business of this government is to circumvent the will of the citizens and allow the nation's children to be discrimated against.</p>
+	    	<p>A government that refuses to enforce the laws of the nation on foreign people while giving financial and legal preferences to those same foreign peoples over US citizens is not a legitimate government. My pledge to the constituents of Texas's congressional district 14 is that I will not allow the government to carry out its business if the business of this government is to circumvent the will of the citizens and allow the nation's children to be discrimated against.</p>
 
 	    	<p>We will not negotiate the application of our laws with the citizens of other nations. We will not engage in a debate concerning the equality of our children.</p>
 
