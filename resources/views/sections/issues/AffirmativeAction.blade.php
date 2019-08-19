dac68a1f11c91f174b139431043a16d5e2b5764b
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-08-18 21:45:53 -0500

    Content Changes

--- resources/views/sections/issues/AffirmativeAction.blade.php
+++ resources/views/sections/issues/AffirmativeAction.blade.php
@@ -3,102 +3,15 @@
 <div class="container clearfix">
 
     <div class="heading-block center nobottomborder">
-        <h2>Civil Rights</h2>
-        <span>A restoration of basic civil rights</span>
+        <h2>Affirmative Action</h2>
+        <span>An immediate end to affirmative action policies</span>
     </div>
 
-    <div class="divider divider-short divider-rounded divider-center">
-        <i class="bgcolor icon-line-head"></i>
-    </div>
-
-    <div class="col_one_third">
-        <div class="feature-box fbox-center fbox-effect">
-            <h3>End the TSA</h3>
-            <p>Stop Terrorists at the border, not children at the airport. Remove the threat to Americans from our countries and do not remove our liberties</p>
-        </div>
-    </div>
-
-    <div class="col_one_third">
-        <div class="feature-box fbox-center fbox-effect">
-            <h3>Protect Political Speech</h3>
-            <p>Violence at political rallies must be ended. Find Antifa's funders and imprison them. Punish violent offenders.</p>
-        </div>
-    </div>
-
-    <div class="col_one_third col_last">
-        <div class="feature-box fbox-center fbox-effect">
-            <h3>Coherent Policies</h3>
-            <p>The rights and duties of companies must be coherent and evenly applied. No separate rules for large and small companies or Christian and nonChristian ones.</p>
-        </div>
-    </div>
-
-    <div class="clear"></div>
-
-    <p>This page covers civil rights issues not relating to the equality of race in America and to immigration. The denial of civil rights to white Americans is covered under the equality sections, but are recognized as basic civil rights issues. This page is more concerned with the civil rights violations that have manifested in the past few decades as either the result of the war on terror or of the cultural marxism attack on Christianity.</p>
-
-    <div class="fancy-title nobottommargin">
-        <h3>End the TSA</h3>
-    </div>
-
-    <div class="divider divider-border divider-right notopmargin">
-        <i class="icon-line-head"></i>
-    </div>
 
     <div class="alignleft">
-        <a href="#"><img src="/images/issues/civilrights.jpg"></a>
+        <a href="#"><img src="/images/issues/affirmativeaction.jpg"></a>
     </div>
 
-    <p>We currently live in a nation that tolerates tens of millions of people living illegally within its boundaries. We have no records of who these people are, what their nations of origin are, or what makes up their criminal background. At the same time, we have a no-fly list for our citizens, and we pat down nuns, children, and WWII veterans before they get on an airplane. We also monitor every American citizen under the guise of preventing terror. This dichonomy is not defendable nor sustainable.</p>
-
-    <p>Instead of insisting that we allow in people with wholly different cultures, and then monitor both them and us to ensure that they are not terrorists, why not simply allow us to be free. There is no logical benefit to this nation to import people who are hostile to its laws and its people. Stop terrorists at the border, and let Americans travel freely within our nation.</p>
-
-    <p>The cost of the TSA to the American citizen is over $7.5 billion a year. Removing the threat from our country removes the need for the TSA to exist and will save American people from the cost of protecting us from ourselves.</p>
-
-
-    <blockquote>
-        <p>Insead of insisting that we allow in people with wholly different cultures, and then monitor both them and us to ensure that they are not terrorists, why not simply allow us to be free.</p>
-        <footer>Joshua Foxworth</footer>
-    </blockquote>
-
-    <p>At the same time, the mass monitoring of citizens can and should be ended. Our enemies must be kept out of our nation and our citizens must be allowed to communicate freely.</p>
-
-
-
-    <div class="fancy-title nobottommargin">
-        <h3>Protect Political Speech - end AntiFa</h3>
-    </div>
-
-    <div class="divider divider-border divider-right notopmargin">
-        <i class="icon-line-head"></i>
-    </div>
-
-    <div class="alignright">
-        <a href="#"><img src="/images/issues/antifa.jpg"></a>
-    </div>
-
-    <p>The 2016 elections saw a drastic - although largely unreported - rise in political violence. All of this violence was directed at Trump supporters or similar right wing candidates and much of it was racially based. Since that time, we have seen the rise of groups like antifa who traffic in violence. These groups regularly assault peaceful activists and endanger the lives of people simply attending political speeches or rallies.</p>
-
-    <p>Since President Trump's election, the violence of this group and similar groups have grown exponentially. It's members attend rallies clad in all black with their faces hidden. This is done for the sole purpose of assaulting political attendees and making it difficult to identify the individual responsible. The groups acts to commit violence for the sole purpose of shutting down opposing speeches and views and it enjoys a great deal of support from the media.</p>
-
-    <p>In 2017, Eric Clanton approached an unsuspecting person at a conservative event in California and <a href="http://www.zerohedge.com/news/2017-05-25/suspected-berkeley-antifa-bike-lock-attacker-eric-clanton-arrested-assault">beat him over the head with a bike lock</a> in an attempt to murder that man with the hopes of scaring people away from attending future events. My Clanton was actually a professor and California State University - showing how main stream this propensity for violence has spread in to the left culture.</p>
-
-    <p>This organization and any like it must be investigated to determine who is financing this attempted overthrow of the US political system and those people must be brought to justice.</p>
-
-
-
-    <div class="fancy-title nobottommargin">
-        <h3>Coherent Policies</h3>
-    </div>
-
-    <div class="divider divider-border divider-right notopmargin">
-        <i class="icon-line-head"></i>
-    </div>
-
-    <p>The US supreme court is <a href="http://www.foxnews.com/us/2017/06/26/us-supreme-court-to-decide-colorado-gay-wedding-cake-case-timeline-events.html">about to hear a case</a> where a Christian baker was fined for refusing to bake a cake for a gay marriage ceremony. This seems to contradict with that company's right to refuse service to whomever they please. It also ignores the fact that the couple could have acquired a cake from multiple other bakers in area and the fact that several muslim bakeries also regularly refuse to bake cakes for gay weddings.</p>
-
-    <p>At the same time, platforms such as google, youTube, twitter, facebook, patreon, and paypal are denying services to entities based upon political views. In this case, these compannies represent a single point of access for people to put out their political views and the deplatforming of those people is far more akin to denial of basic rights than the refusal to bake a cake. I believe that any company should be allowed to refuse service to anyone. However, what we have now is a situation where left wing organizations are free to deny service to right wing people, but Christians and other right wing people are not given that same right.</p>
-
-    <p>There is an obvious need to clarify laws with respect to religion and with respect to the right of a company to sell their labor to whomever they see fit. We cannot say that twitter has a right to refuse service to anyone they see fit, but a family owned bakery must submit to the demands of any person that walks through the door.</p>
-
+    <p>.</p>
 
 </div>
