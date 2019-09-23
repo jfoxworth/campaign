b9b43b55dc085cd2931268d46e539fb6b673aeff
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-09-23 00:46:47 -0500

    add versus

--- resources/views/sections/versus/main.blade.php
+++ resources/views/sections/versus/main.blade.php
@@ -6,28 +6,32 @@
 		
 
 		<!-- Row with images -->
-		<div class="col_one_third"></div>
+		<div class="col_one_third topmargin-lg"></div>
 
-		<div class="col_one_third"><img src="/Images/Foxworth.jpg"></div>
+		<div class="col_one_third topmargin-lg"><img src="/images/foxworth.jpg"></div>
 
-		<div class="col_one_third col_last"><img src="/Images/Weber.jpg"></div>
+		<div class="col_one_third topmargin-lg col_last"><img src="/images/weber.jpg"></div>
 
 
 		<!-- Row with names -->
 		<div class="col_one_third"></div>
 
-		<div class="col_one_third">Joshua Foxworth</div>
+		<div class="col_one_third h2 notopmargin">Joshua Foxworth</div>
 
-		<div class="col_one_third col_last">Congressman Weber</div>
+		<div class="col_one_third h2 notopmargin col_last">Congressman Weber</div>
 
 
 
 		<!-- Illegal Immigration -->
-		<div class="col_one_third">Illegal Immigration</div>
+		<div class="row" style="background-color:#ccc">
 
-		<div class="col_one_third">Pledging to end it <br> on day 1</div>
+			<div class="col_one_third h3" style="padding:10px">Illegal Immigration</div>
 
-		<div class="col_one_third col_last">8 years <br>No action taken</div>
+			<div class="col_one_third h3" style="padding:10px">Pledging to end it <br> on day 1</div>
+
+			<div class="col_one_third h3 col_last" style="padding:10px">8 years <br>No action taken</div>
+
+		</div>
 
 
 
