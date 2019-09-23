922e4ccaf7afc776689ecd967c75cbb39232356a
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-09-23 01:45:53 -0500

    add versus

--- resources/views/sections/versus/main.blade.php
+++ resources/views/sections/versus/main.blade.php
@@ -35,11 +35,11 @@
 		<!-- Illegal Immigration -->
 		<div class="row" style="background-color:#ccc">
 
-			<div class="col_one_third h3" style="padding:10px; margin-top:30px">Illegal Immigration</div>
+			<div class="col_one_third h3" style="padding:10px;  margin-top:30px; margin-bottom:20px;">Illegal Immigration</div>
 
-			<div class="col_one_third h3" style="padding:10px">Pledging to end it <br> on day 1</div>
+			<div class="col_one_third h3" style="padding:10px; margin-top:30px; margin-bottom:20px;">Pledging to end it <br> on day 1</div>
 
-			<div class="col_one_third h3 col_last" style="padding:10px">8 years <br>No action taken</div>
+			<div class="col_one_third h3 col_last" style="padding:10px; margin-top:30px; margin-bottom:20px;">8 years <br>No action taken</div>
 
 		</div>
 
@@ -47,11 +47,11 @@
 		<!-- Affirmative Action -->
 		<div class="row">
 
-			<div class="col_one_third h3" style="padding:10px; margin-top:30px">Affirmative Action</div>
+			<div class="col_one_third h3" style="padding:10px; margin-top:30px; margin-bottom:20px;">Affirmative Action</div>
 
-			<div class="col_one_third h3" style="padding:10px">Pledging to end it <br> on day 1</div>
+			<div class="col_one_third h3" style="padding:10px; margin-top:30px; margin-bottom:20px;">Pledging to end it <br> on day 1</div>
 
-			<div class="col_one_third h3 col_last" style="padding:10px">8 years <br>No position taken</div>
+			<div class="col_one_third h3 col_last" style="padding:10px; margin-top:30px; margin-bottom:20px;">8 years <br>No position taken</div>
 
 		</div>
 
@@ -61,11 +61,11 @@
 		<!-- H1B Visas -->
 		<div class="row" style="background-color:#ccc">
 
-			<div class="col_one_third h3" style="padding:10px;  margin-top:30px">H1B Visas</div>
+			<div class="col_one_third h3" style="padding:10px;  margin-top:30px; margin-bottom:20px;">H1B Visas</div>
 
-			<div class="col_one_third h3" style="padding:10px">Program should<br>be ended</div>
+			<div class="col_one_third h3" style="padding:10px;  margin-top:30px; margin-bottom:20px;">Program should<br>be ended</div>
 
-			<div class="col_one_third h3 col_last" style="padding:10px">Voted for virtually<br>unlimited H1B Visas<br>from India / Pakistan</div>
+			<div class="col_one_third h3 col_last" style="padding:10px;  margin-top:30px; margin-bottom:20px;">Voted for virtually<br>unlimited H1B Visas<br>from India / Pakistan</div>
 
 		</div>
 
@@ -75,11 +75,11 @@
 		<!-- Legal Migration -->
 		<div class="row">
 
-			<div class="col_one_third h3" style="padding:10px;  margin-top:30px">Legal Migration</div>
+			<div class="col_one_third h3" style="padding:10px;  margin-top:30px; margin-bottom:20px;">Legal Migration</div>
 
-			<div class="col_one_third h3" style="padding:10px">Drastic reform<br>End H1B, H2B, EB5, etc<br>Long Moratorium</div>
+			<div class="col_one_third h3" style="padding:10px; margin-top:30px; margin-bottom:20px;">Drastic reform<br>End H1B, H2B, EB5, etc<br>Long Moratorium</div>
 
-			<div class="col_one_third h3 col_last" style="padding:10px">Supports restructuring<br>legal process</div>
+			<div class="col_one_third h3 col_last" style="padding:10px;  margin-top:30px; margin-bottom:20px;">Supports restructuring<br>legal process</div>
 
 		</div>
 
@@ -89,11 +89,11 @@
 		<!-- TAPS -->
 		<div class="row" style="background-color:#ccc">
 
-			<div class="col_one_third h3" style="padding:10px;  margin-top:30px">TAPS Threat Assessment</div>
+			<div class="col_one_third h3" style="padding:10px;  margin-top:30px; margin-bottom:20px;">TAPS Threat Assessment</div>
 
-			<div class="col_one_third h3" style="padding:10px">Opposes</div>
+			<div class="col_one_third h3" style="padding:10px;  margin-top:30px; margin-bottom:20px;">Opposes</div>
 
-			<div class="col_one_third h3 col_last" style="padding:10px">Cosponsored Legislation</div>
+			<div class="col_one_third h3 col_last" style="padding:10px;  margin-top:30px; margin-bottom:20px;">Cosponsored Legislation</div>
 
 		</div>
 
@@ -103,11 +103,11 @@
 		<!-- Domestic Terrorists -->
 		<div class="row">
 
-			<div class="col_one_third h3" style="padding:10px;  margin-top:30px">Labeling Americans<br>"Domestic Terrorists"</div>
+			<div class="col_one_third h3" style="padding:10px;  margin-top:30px; margin-bottom:20px;">Labeling Americans<br>"Domestic Terrorists"</div>
 
-			<div class="col_one_third h3" style="padding:10px">Never - Will<br>be used against patriots</div>
+			<div class="col_one_third h3" style="padding:10px;  margin-top:30px; margin-bottom:20px;">Never - Will<br>be used against patriots</div>
 
-			<div class="col_one_third h3 col_last" style="padding:10px">Introduced Legislation<br>with Dem Congressman</div>
+			<div class="col_one_third h3 col_last" style="padding:10px;  margin-top:30px; margin-bottom:20px;">Introduced Legislation<br>with Dem Congressman</div>
 
 		</div>
 
@@ -118,11 +118,11 @@
 		<!-- Tech Bill of Rights -->
 		<div class="row" style="background-color:#ccc">
 
-			<div class="col_one_third h3" style="padding:10px;  margin-top:30px">Tech Bill of Rights</div>
+			<div class="col_one_third h3" style="padding:10px; margin-top:30px; margin-bottom:20px;">Tech Bill of Rights</div>
 
-			<div class="col_one_third h3" style="padding:10px">This is needed<br>immediately</div>
+			<div class="col_one_third h3" style="padding:10px;  margin-top:30px; margin-bottom:20px;">This is needed<br>immediately</div>
 
-			<div class="col_one_third h3 col_last" style="padding:10px">No stated position</div>
+			<div class="col_one_third h3 col_last" style="padding:10px; margin-top:30px; margin-bottom:20px;">No stated position</div>
 
 		</div>
 
@@ -133,9 +133,9 @@
 
 			<div class="col_one_third"></div>
 
-			<div class="col_one_third h2 notopmargin bottommargin-sm">Joshua Foxworth</div>
+			<div class="col_one_third h2" style="padding:10px;  margin-top:30px">Joshua Foxworth</div>
 
-			<div class="col_one_third h2 notopmargin bottommargin-sm col_last">Congressman Weber</div>
+			<div class="col_one_third h2"  style="padding:10px;  margin-top:30px">Congressman Weber</div>
 
 		</div>
 
