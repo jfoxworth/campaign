918c806bc5026529feee95a1f677d77e2930c031
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-09-23 01:07:58 -0500

    add versus

--- resources/views/sections/versus/main.blade.php
+++ resources/views/sections/versus/main.blade.php
@@ -6,19 +6,29 @@
 		
 
 		<!-- Row with images -->
-		<div class="col_one_third topmargin-lg"></div>
+		<div class="row">
 
-		<div class="col_one_third topmargin-lg"><img src="/images/foxworth.jpg"></div>
+			<div class="col_one_third topmargin-lg"></div>
 
-		<div class="col_one_third topmargin-lg col_last"><img src="/images/weber.jpg"></div>
+			<div class="col_one_third topmargin-lg"><img src="/images/foxworth.jpg"></div>
+
+			<div class="col_one_third topmargin-lg col_last"><img src="/images/weber.jpg"></div>
+
+		</div>
+
+
 
 
 		<!-- Row with names -->
-		<div class="col_one_third"></div>
+		<div class="row">
 
-		<div class="col_one_third h2 notopmargin">Joshua Foxworth</div>
+			<div class="col_one_third"></div>
 
-		<div class="col_one_third h2 notopmargin col_last">Congressman Weber</div>
+			<div class="col_one_third h2 notopmargin bottommargin-sm">Joshua Foxworth</div>
+
+			<div class="col_one_third h2 notopmargin bottommargin-sm col_last">Congressman Weber</div>
+
+		</div>
 
 
 
@@ -32,6 +42,34 @@
 			<div class="col_one_third h3 col_last" style="padding:10px">8 years <br>No action taken</div>
 
 		</div>
+
+
+		<!-- Affirmative Action -->
+		<div class="row">
+
+			<div class="col_one_third h3" style="padding:10px">Affirmative Action</div>
+
+			<div class="col_one_third h3" style="padding:10px">Pledging to end it <br> on day 1</div>
+
+			<div class="col_one_third h3 col_last" style="padding:10px">8 years <br>No position taken</div>
+
+		</div>
+
+
+
+
+		<!-- H1B Visas -->
+		<div class="row" style="background-color:#ccc">
+
+			<div class="col_one_third h3" style="padding:10px">H1B Visas</div>
+
+			<div class="col_one_third h3" style="padding:10px">Program should<br>be ended</div>
+
+			<div class="col_one_third h3 col_last" style="padding:10px">Voted for virtually<br>unlimited H1B Visas<br>from India / Pakistan</div>
+
+		</div>
+
+
 
 
 
