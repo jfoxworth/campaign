4087008c2987a1b2d1a98449df7a55c74acc073c
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-09-26 10:07:21 -0500

    add versus

--- resources/views/sections/landing/donaterow.blade.php
+++ resources/views/sections/landing/donaterow.blade.php
@@ -1,4 +1,4 @@
-<a class="button button-full center tright href="/donate">
+<a class="button button-full center tright" href="/donate">
     <div class="container clearfix">
         Support the campaign to restore our nation <strong> - Donations - </strong> <i class="icon-caret-right" style="top:4px;"></i>
     </div>
