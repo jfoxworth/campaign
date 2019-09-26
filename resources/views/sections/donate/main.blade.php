8c79ac5c950e84d2641f1f9714bf7016a4f30432
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-09-25 23:00:34 -0500

    add versus

--- resources/views/sections/donate/main.blade.php
+++ resources/views/sections/donate/main.blade.php
@@ -18,7 +18,9 @@
 
 			<p>Congressional campaigns can be very expensive. I don't believe that they have to be. I am going to knock on thousands of doors and meet with thousands more.</p>
 
-			<p>That being said, I will need money for campaign material, facebook ads, housing and food for volunteers, and potentially radio ads. Once we raise more than the amount below, we will focus on additional items. However, here is what your money will go towards right now if you choose to donate.</p>
+			<p>That being said, I will need money for campaign material, facebook ads, housing and food for volunteers, and potentially radio ads. The amounts below are the first "tier" of what I will do with the funds. Once we raise more than this amount, we will focus on additional items. If enough money is raised, we will alter our strategy to more expensive methods of advertising.</p>
+
+			<p>Here is what your money will go towards right now if you choose to donate. This amount is less than $20,000. I believe that I can be competitive with this amount because of the amount of time that I am putting into campaigning as well as the strength of the message. The bottom line is that when I talk to people, many of them share these views and they are simply waiting for someone to support.</p>
 
 		</span>
 
@@ -47,7 +49,7 @@
 		</div>
 
 		<div class="col_one_fourth center">
-			$5,000
+			$2,500
 		</div>
 
 		<div class="col_one_fourth col_last"></div>
@@ -78,26 +80,11 @@
 		</div>
 
 		<div class="col_one_fourth center">
-			$10,000
+			$10,000+
 		</div>
 
 		<div class="col_one_fourth col_last"></div>
 
-
-
-
-		<!--- Phone List -->
-		<div class="col_one_fourth"></div>
-
-		<div class="col_one_fourth center">
-			Phone List
-		</div>
-
-		<div class="col_one_fourth center">
-			$5,000
-		</div>
-
-		<div class="col_one_fourth col_last"></div>
 
 
 
