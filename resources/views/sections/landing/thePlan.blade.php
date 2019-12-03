b335cc3156636b104ae63e6ec0f9e25d8c08e565
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-12-03 13:35:08 -0600

    The plan

--- resources/views/sections/landing/thePlan.blade.php
+++ resources/views/sections/landing/thePlan.blade.php
@@ -1,10 +1,10 @@
-<div class="section clearfix topmargin" style="background-color:#cccccc">
+<div class="section clearfix notopmargin" style="background-color:#cccccc">
 
     <div class="heading-block center ">
         <h2 class="t500 nott" style="font-size:36px:">The Plan</h2>
-        <span>1. Enforce existing laws that fine employers - no new legislation is needed</span>
-        <span>2. Illegal aliens cannot obtain jobs, welfare, or free health care</span>
-        <span>3. Illegal aliens will return to their homes</span>
-        <span>4. Those that remain will be deported when the come into contact with law enforcement</span>
+        <h5>1. Enforce existing laws that fine employers - no new legislation is needed</h5>
+        <h5>2. Illegal aliens cannot obtain jobs, welfare, or free health care</h5>
+        <h5>3. Illegal aliens will return to their homes</h5>
+        <h5>4. Those that remain will be deported when the come into contact with law enforcement</h5>
     </div>
 </div>
