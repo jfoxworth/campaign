6aafc959e73d4361dfbed501bd243cf3fe74138a
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-12-05 13:24:07 -0600

    The plan

--- resources/views/sections/landing/thePlan.blade.php
+++ resources/views/sections/landing/thePlan.blade.php
@@ -5,6 +5,6 @@
         <h4 class="bottommargin-sm">1. Enforce existing laws that fine employers - no new legislation is needed</h4>
         <h4 class="bottommargin-sm">2. Illegal aliens cannot obtain jobs, welfare, or free health care</h4>
         <h4 class="bottommargin-sm">3. Illegal aliens will return to their homes</h4>
-        <h4 class="bottommargin-sm">4. Those that remain will be deported when the come into contact with law enforcement</h4>
+        <h4 class="bottommargin-sm">4. Those that remain will be deported when they come into contact with law enforcement</h4>
     </div>
 </div>
