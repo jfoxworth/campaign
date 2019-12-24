64d047182e03275a519bcbb9e192deb4c9669e6f
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-12-24 12:17:26 -0600

    landing changes

--- resources/views/sections/landing/callIn.blade.php
+++ resources/views/sections/landing/callIn.blade.php
@@ -8,7 +8,8 @@
 
             <div>
 
-            	<p style="font-size:16px">I hold telephone town halls every Thursday evening at 8pm. You can join us using <a href="https://meet.google.com/cir-cpef-rbf">google hangouts</a> or by calling +1 442-600-5878‬ and using the pin ‪788 638 033‬#</p>
+            	<p style="font-size:20px">I hold telephone town halls every Thursday evening at 8pm. You can join us using <a href="https://meet.google.com/cir-cpef-rbf">google hangouts</a> or by calling +1 442-600-5878‬ and using the pin ‪788 638 033‬#</p>
+
             </div>
 
         </div>
