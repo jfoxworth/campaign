88b2a2679152539296c9b0a89a803892362b3d83
Joshua Foxworth <jfoxworth@cadwolf.com>
2020-01-10 08:53:20 -0600

    events section

---
+++ resources/views/sections/landing/events.blade.php
@@ -0,0 +1,118 @@
+<div class="section nomargin">
+
+    <div class="container clearfix">
+
+        <div class="col-md-12">
+
+            <div class="col-md-6 center">
+
+				<h2 class="center">Events Where I will be Speaking</h2>
+
+
+				<div class="ipost clearfix bottommargin-sm">
+
+					<div class="col_half nobottommargin">
+
+						<div class="entry-image">
+							<img class="image_fade" src="images/events/RNGLogo.png" alt="RNG" style="opacity: 1;">
+						</div>
+
+					</div>
+
+					<div class="col_half nobottommargin col_last">
+
+						<div class="entry-title">
+							<h3>Republican Networking Group Forum</h3>
+						</div>
+
+						<ul class="entry-meta clearfix">
+							<li><i class="icon-calendar3"></i> 16th Jan 2020 - 6pm</li>
+						</ul>
+
+						<div class="entry-content">
+							<p>This will be a smaller setting with other races talking. There will be a brief intro by the candidates and then questions from the audience.</p>
+						</div>
+
+					</div>
+
+				</div>
+
+
+
+
+
+				<div class="ipost clearfix bottommargin-sm">
+
+					<div class="col_half nobottommargin">
+
+						<div class="entry-image">
+							<img class="image_fade" src="images/events/TEAForum.png" alt="TEA" style="opacity: 1;">
+						</div>
+
+					</div>
+
+
+					<div class="col_half nobottommargin col_last">
+
+						<div class="entry-title">
+							<h3>TEA Party Forum</h3>
+						</div>
+
+						<ul class="entry-meta clearfix">
+							<li><i class="icon-calendar3"></i> 20th Jan 2020 - 7pm</li>
+						</ul>
+
+						<div class="entry-content">
+							<p>This forum will be held for a number of races, including the US House race and the County Chair and two Constable races. </p>
+						</div>
+
+					</div>
+
+
+				</div>
+
+
+
+
+
+
+
+
+				<div class="ipost clearfix bottommargin-sm">
+
+					<div class="col_half nobottommargin">
+
+						<div class="entry-image">
+							<img class="image_fade" src="images/events/phone.jpg" alt="Phone Meeting" style="opacity: 1;">
+						</div>
+
+					</div>
+
+
+					<div class="col_half nobottommargin col_last">
+
+						<div class="entry-title">
+							<h3>Weekly Town Halls</h3>
+						</div>
+
+						<ul class="entry-meta clearfix">
+							<li><i class="icon-calendar3"></i> Every Thursday at 8pm</li>
+						</ul>
+
+						<div class="entry-content">
+							<p>I hold weekly telephone town halls. I introduce myself and my platform and then take questions. You can use the <a href="">google hangout</a> or call this number - 1 442-600-5878‬ - and enter this pin - 788 638 033‬#</p>
+						</div>
+
+					</div>
+
+
+				</div>
+
+
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
---
+++ resources/views/sections/landing/votingDates.blade.php
@@ -0,0 +1,7 @@
+<div class="section dark clearfix topmargin nobottommargin">
+
+    <div class="heading-block center ">
+        <h2 class="t500 nott" style="font-size:36px:"><span>Voting Dates</span></h2>
+        <span>Early voting for the election will be February 18-28 and the final vote will be March 3. </span>
+    </div>
+</div>
