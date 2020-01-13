d1885dd2640cce7b508918d235efb6c50fa41c9a
Joshua Foxworth <jfoxworth@cadwolf.com>
2020-01-13 02:13:21 -0600

    events section

--- resources/views/sections/media/main.blade.php
+++ resources/views/sections/media/main.blade.php
@@ -26,7 +26,7 @@
 					</ul>
 				
 					<div class="entry-content mt-0">
-						<p>This article discusses how twitter's cancelling of </p>
+						<p>This article discusses how twitter's cancelling of political ads is just another avenue of preventing the free speech of conservatives and right wingers in America.</p>
 					</div>
 
 				</div>
@@ -53,6 +53,10 @@
 						<li><span>Published by</span> <a href="https://www.americanthinker.com/blog/2019/10/this_bipartisan_immigration_bill_will_change_the_face_of_america.html">American Thinker</a></li>
 						<li><i class="icon-time"></i><a href="https://www.americanthinker.com/blog/2019/10/this_bipartisan_immigration_bill_will_change_the_face_of_america.html">10 Oct 2019</a></li>
 					</ul>
+
+					<div class="entry-content mt-0">
+						<p>This is a discussion of HR 1044 that I wrote not too long after it was passed in the House. That bill removes the caps on the H1B program that limit individual nations.</p>
+					</div>
 				
 				</div>
 			
@@ -79,6 +83,12 @@
 						<li><span>Published by</span> <a href="https://www.americanthinker.com/articles/2019/09/affirmative_action_and_nasa.html">American Thinker</a></li>
 						<li><i class="icon-time"></i><a href="https://www.americanthinker.com/articles/2019/09/affirmative_action_and_nasa.html">31 Oct 2019</a></li>
 					</ul>
+
+
+					<div class="entry-content mt-0">
+						<p>This article addresses the affirmative action policies that are in place at NASA and how they affect that institution.</p>
+					</div>
+
 				
 				</div>
 			
@@ -92,18 +102,26 @@
 
 
 			<article class="spost clearfix">
+
+				<div class="entry-image">
+					<a href="#"></a>
+				</div>
 			
 				<div class="entry-c">
 			
 					<div class="entry-title">
-						<h4 class="t600"><a href="#"></a></h4>
+						<h4 class="t600"><a href="https://www.americanthinker.com/blog/2019/09/republicans_must_stem_the_tide_of_illegal_immigration_or_be_obliterated.html">Republicans must stem the tide of illegal immigration or be obliterated</a></h4>
 					</div>
 			
 					<ul class="entry-meta clearfix">
-						<li><span>Published by</span> <a href="#">American Thinker</a></li>
-						<li><i class="icon-time"></i><a href="#">31 Oct 2019</a></li>
+						<li><span>Published by</span> <a href="https://www.americanthinker.com/blog/2019/09/republicans_must_stem_the_tide_of_illegal_immigration_or_be_obliterated.html">American Thinker</a></li>
+						<li><i class="icon-time"></i><a href="https://www.americanthinker.com/blog/2019/09/republicans_must_stem_the_tide_of_illegal_immigration_or_be_obliterated.html">16 Sept 2019</a></li>
 					</ul>
 				
+					<div class="entry-content mt-0">
+						<p>This is a discussion of how demographics are changed by illegal immigration and how it will lead to the end of the Republican party.</p>
+					</div>
+
 				</div>
 			
 			</article>
@@ -117,6 +135,98 @@
 
 
 			<article class="spost clearfix">
+
+				<div class="entry-image">
+					<a href="#"></a>
+				</div>
+			
+				<div class="entry-c">
+			
+					<div class="entry-title">
+						<h4 class="t600"><a href="https://www.americanthinker.com/articles/2019/09/make_no_mistake_the_gop_is_losing_texas.html">Make no mistake: The GOP is losing Texas</a></h4>
+					</div>
+			
+					<ul class="entry-meta clearfix">
+						<li><span>Published by</span> <a href="https://www.americanthinker.com/articles/2019/09/make_no_mistake_the_gop_is_losing_texas.html">American Thinker</a></li>
+						<li><i class="icon-time"></i><a href="https://www.americanthinker.com/articles/2019/09/make_no_mistake_the_gop_is_losing_texas.html">13 Sept 2019</a></li>
+					</ul>
+
+					<div class="entry-content mt-0">
+						<p>This is more in depth discussion of the demographics in Texas and highlighting a number of districts in Texas and what is happening in those districts.</p>
+					</div>
+				
+				</div>
+			
+			</article>
+
+
+
+
+
+
+			<article class="spost clearfix">
+
+				<div class="entry-image">
+					<a href="#"></a>
+				</div>
+			
+				<div class="entry-c">
+			
+					<div class="entry-title">
+						<h4 class="t600"><a href="https://www.americanthinker.com/articles/2019/08/unidirectional_empathy_is_now_the_cornerstone_of_modern_society.html">Unidirectional empathy is now the cornerstone of modern society</a></h4>
+					</div>
+			
+					<ul class="entry-meta clearfix">
+						<li><span>Published by</span> <a href="https://www.americanthinker.com/articles/2019/08/unidirectional_empathy_is_now_the_cornerstone_of_modern_society.html">American Thinker</a></li>
+						<li><i class="icon-time"></i><a href="https://www.americanthinker.com/articles/2019/08/unidirectional_empathy_is_now_the_cornerstone_of_modern_society.html">6 Aug 2019</a></li>
+					</ul>
+
+					<div class="entry-content mt-0">
+						<p>This article discusses a number of issues that convey the message that empathy is demanded from America towards all other nations, but none is accepted towards us.</p>
+					</div>
+				
+				</div>
+			
+			</article>
+
+
+
+
+
+			<article class="spost clearfix">
+
+				<div class="entry-image">
+					<a href="#"></a>
+				</div>
+			
+				<div class="entry-c">
+			
+					<div class="entry-title">
+						<h4 class="t600"><a href="https://www.americanthinker.com/articles/2019/07/nationalistic_college_debt_relief.html">Nationalistic college debt relief</a></h4>
+					</div>
+			
+					<ul class="entry-meta clearfix">
+						<li><span>Published by</span> <a href="https://www.americanthinker.com/articles/2019/07/nationalistic_college_debt_relief.html">American Thinker</a></li>
+						<li><i class="icon-time"></i><a href="https://www.americanthinker.com/articles/2019/07/nationalistic_college_debt_relief.html">5 July 2019</a></li>
+					</ul>
+
+					<div class="entry-content mt-0">
+						<p>At the time this article was written, a number of platforms were addressing the college debt crisis. This is my plan to address it.</p>
+					</div>
+				
+				</div>
+			
+			</article>
+
+
+
+
+
+			<article class="spost clearfix">
+
+				<div class="entry-image">
+					<a href="#"></a>
+				</div>
 			
 				<div class="entry-c">
 			
@@ -128,11 +238,14 @@
 						<li><span>Published by</span> <a href="#">American Thinker</a></li>
 						<li><i class="icon-time"></i><a href="#">31 Oct 2019</a></li>
 					</ul>
+
+					<div class="entry-content mt-0">
+						<p></p>
+					</div>
 				
 				</div>
 			
 			</article>
-
 
 
 		</span>
