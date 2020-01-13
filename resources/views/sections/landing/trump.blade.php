f5aaaf20320474b55ec5d0cf185c628879f6756d
Joshua Foxworth <jfoxworth@cadwolf.com>
2020-01-13 02:58:02 -0600

    events section

--- resources/views/sections/landing/trump.blade.php
+++ resources/views/sections/landing/trump.blade.php
@@ -1,8 +1,8 @@
 <div class="section dark clearfix topmargin nobottommargin">
 
     <div class="heading-block center ">
-        <span>I am the only candidate in the race that endorsed and supported<br>
-        	 President Trump prior the November 2016 election. I did this because<br>
-        	illegal immigration must be addressed and it must be addressed immediately.</span>
+        <h4>I am the only candidate in the race that endorsed and supported<br>
+        	 President Trump <span>prior the November 2016 election</span>. I did this because<br>
+        	illegal immigration must be addressed and it must be addressed immediately.</h4>
     </div>
 </div>
