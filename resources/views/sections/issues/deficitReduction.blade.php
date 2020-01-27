fac5a7ed0e8d1602219e9d9612ededd485731b5b
Joshua Foxworth <jfoxworth@cadwolf.com>
2020-01-27 14:13:18 -0600

    issues fixes

--- resources/views/sections/issues/deficitReduction.blade.php
+++ resources/views/sections/issues/deficitReduction.blade.php
@@ -9,14 +9,21 @@
 
     <div class="clear"></div>
 
-    <div class="fancy-title nobottommargin topmargin">
-        <h3>The current deficit</h3>
-    </div>
-
     <div class="divider divider-short divider-rounded divider-center">
         <i class="bgcolor icon-money"></i>
     </div>
 
+    <div class="clear"></div>
+
+
+
+
+
+
+
+    <div class="fancy-title nobottommargin topmargin">
+        <h3>The current deficit</h3>
+    </div>
 
 
     <p>Less than a decade ago, the House Republicans pushed for a balanced budget amendment. Today, the yearly deficit is more than one trillion dollars, the total debt is over $23 Trillion, and even at historically low interest rates, we are spending around 5% of our budget on interest payments on the debt.</p>
@@ -26,12 +33,17 @@
 
 
 
+
+
+
+
+
+
+
+
+
     <div class="fancy-title nobottommargin topmargin">
         <h3>One plan - eliminate costs, lower bureaucracy, repurpose the military</h3>
-    </div>
-
-    <div class="divider divider-short divider-rounded divider-center">
-        <i class="bgcolor icon-money"></i>
     </div>
 
 
@@ -50,13 +62,31 @@
 
 
 
-    <div class="fancy-title nobottommargin topmargin">
-        <h3>Immigration is largest drive of costs</h3>
-    </div>
+
+
+
+
+
+    <div class="clear"></div>
 
     <div class="divider divider-short divider-rounded divider-center">
         <i class="bgcolor icon-money"></i>
     </div>
+
+    <div class="clear"></div>
+
+
+
+
+
+
+
+
+
+    <div class="fancy-title nobottommargin topmargin">
+        <h3>Immigration is largest drive of costs</h3>
+    </div>
+
 
 
     <p>The dramatic expansion of both legal and illegal immigration into the US over the past few decades has driven an equally dramatic rise in the need for services. However, this same increase in immigration has driven down wages and placed a higher percentage of people below the line where they are now paying less into the system than they are taking out of it. Ending illegal immigration would lower costs, increase wages, and result in a massive upswing in the amount of money being brought in per person within the US.</p>
@@ -73,10 +103,6 @@
 
     <div class="fancy-title nobottommargin topmargin">
         <h3>Ending affirmative action</h3>
-    </div>
-
-    <div class="divider divider-short divider-rounded divider-center">
-        <i class="bgcolor icon-money"></i>
     </div>
 
 
@@ -102,10 +128,6 @@
         <h3>Repurpose the military</h3>
     </div>
 
-    <div class="divider divider-short divider-rounded divider-center">
-        <i class="bgcolor icon-money"></i>
-    </div>
-
 
     <p>The amount of money that the US spends on our military is not sustainable and not needed. We should withdraw our forces from Iraq and Iran and place our military on our southern border. We should reduce the number of US bases overseas and focus on retaining the highest quality Solders, Sailors, Airmen, and Marines. We should focus on advancing our military technology and eliminating our dependence on Chinese products.</p>
 
@@ -127,10 +149,6 @@
 
     <div class="fancy-title nobottommargin topmargin">
         <h3>End unneeded bureacracies</h3>
-    </div>
-
-    <div class="divider divider-short divider-rounded divider-center">
-        <i class="bgcolor icon-money"></i>
     </div>
 
 
@@ -157,10 +175,6 @@
         <h3>Address prescription drugs</h3>
     </div>
 
-    <div class="divider divider-short divider-rounded divider-center">
-        <i class="bgcolor icon-money"></i>
-    </div>
-
 
     <p>The United States allows pharmaceutical companies to sell drugs overseas at a fraction of the cost of what they are sold for here in the US. Eliminating this will lower the short and long term costs for US health care in the US. Allowing the US government to negotiate lower prices would also help. One of the primary drivers of our unfunded liabilities are these prescription drug costs. Addressing them will greatly improve our prosects for the future.</p>
 
@@ -182,10 +196,6 @@
 
     <div class="fancy-title nobottommargin topmargin">
         <h3>Audit the FED and end QE</h3>
-    </div>
-
-    <div class="divider divider-short divider-rounded divider-center">
-        <i class="bgcolor icon-money"></i>
     </div>
 
 
@@ -212,10 +222,6 @@
 
     <div class="fancy-title nobottommargin topmargin">
         <h3>Address welfare and social security</h3>
-    </div>
-
-    <div class="divider divider-short divider-rounded divider-center">
-        <i class="bgcolor icon-money"></i>
     </div>
 
 
@@ -247,10 +253,6 @@
         <h3>This is merely a start</h3>
     </div>
 
-    <div class="divider divider-short divider-rounded divider-center">
-        <i class="bgcolor icon-money"></i>
-    </div>
-
 
     <p>Many of these things would have drastic effects on the economy of the US. Once these items are put in place, we would then reassess the situation and see what cuts need to be made. However, the simple reality is that you cannot balance a budget when 10%-15% of your population are foreign citizens living illegally within your borders.</p>
 
@@ -266,10 +268,6 @@
 
     <div class="fancy-title nobottommargin topmargin">
         <h3>Joshua Foxworth versus Congressman Weber</h3>
-    </div>
-
-    <div class="divider divider-border divider-right notopmargin">
-        <i class="icon-line-target"></i>
     </div>
 
     <p>Congressman Weber has put forth no plan nor endorsed any plan to address the deficit.</p>
